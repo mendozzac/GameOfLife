0eb72ccd76417564533ae419f95eba71ce13f0c7
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-10-09 18:47:31 +0200

    add cuadrado function: new array

--- src/juego.js
+++ src/juego.js
@@ -1,1 +1,7 @@
-
+function cuadrado(filas, columnas) {
+  const objetoBase = new Array(filas);
+  for (i = 0; i < filas; i++) {
+    objetoBase[i] = new Array(columnas);
+  }
+  return objetoBase;
+}
