5e9c08538dbf2c3ffb1d014bacd4ed11b00c1a1f
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-10-10 09:41:46 +0200

    add neighbors: añadirVecinos function

--- js/canvas.js
+++ js/canvas.js
@@ -13,4 +13,13 @@ const celulas = function (x, y, estado) {
   this.x = x;
   this.y = y;
   this.estadoProximo = estado;
+
+  this.añadirVecinos = function () {
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        const vecinoX = (this.x + j + columnas) % columnas;
+        const vecinoY = (this.y + i + filas) % filas;
+      }
+    }
+  };
 };
