4d4a324885ad2253030990b4aadaec27c264f601
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-10-10 10:19:02 +0200

    exclude me of neighbors

--- js/canvas.js
+++ js/canvas.js
@@ -5,6 +5,17 @@ const canvasX = 500;
 const canvasY = 500;
 const filas = 100;
 const columnas = 100;
+const casillaX = Math.floor(canvasX / filas);
+const casillaY = Math.floor(canvasY / columnas);
+const tablero = cuadrado();
+
+function cuadrado(filas, columnas) {
+  const objetoBase = new Array(filas);
+  for (let i = 0; i < filas; i++) {
+    objetoBase[i] = new Array(columnas);
+  }
+  return objetoBase;
+}
 
 canvas.width = canvasX;
 canvas.height = canvasY;
@@ -13,12 +24,17 @@ const celulas = function (x, y, estado) {
   this.x = x;
   this.y = y;
   this.estadoProximo = estado;
+  this.vecinos = [];
 
   this.añadirVecinos = function () {
     for (let i = -1; i < 2; i++) {
       for (let j = -1; j < 2; j++) {
         const vecinoX = (this.x + j + columnas) % columnas;
         const vecinoY = (this.y + i + filas) % filas;
+
+        if (i !== 0 || j !== 0) {
+          this.vecinos.push(tablero[vecinoY][vecinoX]);
+        }
       }
     }
   };
