6cd8a2f749e6a1b0246b8afe2e911d3a40e7692f
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-10-10 09:36:46 +0200

    define cells

--- js/canvas.js
+++ js/canvas.js
@@ -5,5 +5,12 @@ const canvasX = 500;
 const canvasY = 500;
 const filas = 100;
 const columnas = 100;
+
 canvas.width = canvasX;
 canvas.height = canvasY;
+
+const celulas = function (x, y, estado) {
+  this.x = x;
+  this.y = y;
+  this.estadoProximo = estado;
+};
